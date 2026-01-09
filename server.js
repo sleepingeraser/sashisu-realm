6cca88088675945e6ca6b0bdb4acdd081a15599c
sleepingeraser <windyandmelody2005@gmail.com>
2026-01-10 04:29:45 +0800

    updated

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(
       "http://localhost:5500",
       "http://127.0.0.1:5500",
       "http://localhost:3000",
-      "https://github.com/sleepingeraser",
+      "https://sleepingeraser.github.io",
       "https://github.com/sleepingeraser/sashisu-realm",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
