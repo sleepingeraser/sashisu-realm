4286f02c2325e4e6724695c2ac22fedeca335f21
sleepingeraser <windyandmelody2005@gmail.com>
2026-01-09 13:52:35 +0800

    backend

--- server.js
+++ server.js
@@ -95,6 +95,6 @@ app.get("/api/products", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server runningon port ${PORT}`);
 });
