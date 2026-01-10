965c514dcc77957d1f1838d682f4f0aac67a3b19
sleepingeraser <windyandmelody2005@gmail.com>
2026-01-11 07:04:24 +0800

    backend order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const orderController = require("../controllers/orderController");
 const authMiddleware = require("../middleware/authMiddleware");
 
+// create new order (with points or card payment)
 router.post("/", authMiddleware, orderController.create);
+
+// get user's orders
 router.get("/", authMiddleware, orderController.listMine);
 
 module.exports = router;
